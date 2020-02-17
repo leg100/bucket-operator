ac3d62c5f32feb951351341161294ec46d0216c4
Louis Garman <louisgarman@gmail.com>
2020-02-17 12:18:39 +0000

    Scaffolding

---
+++ pkg/apis/goalspike/group.go
@@ -0,0 +1,6 @@
+// Package goalspike contains goalspike API versions.
+//
+// This file ensures Go source parsers acknowledge the goalspike package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package goalspike
