44730531c9e80c91f0f35ca006387e6f8cb9d196
Louis Garman <louisgarman@gmail.com>
2020-02-19 17:26:22 +0000

    CRD spec fields

--- pkg/apis/goalspike/v1alpha1/bucket_types.go
+++ pkg/apis/goalspike/v1alpha1/bucket_types.go
@@ -12,6 +12,8 @@ type BucketSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Name    string `json:"name"`
+	Project string `json:"project"`
 }
 
 // BucketStatus defines the observed state of Bucket
